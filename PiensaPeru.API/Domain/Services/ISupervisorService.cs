513b63c63d96d2a13bfa6e534d964cb0639dcf70
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-12 21:09:47 -0500

    Add Services Interfaces

---
+++ PiensaPeru.API/Domain/Services/ISupervisorService.cs
@@ -0,0 +1,13 @@
+﻿using PiensaPeru.API.Domain.Models;
+using PiensaPeru.API.Domain.Services.Communications;
+
+namespace PiensaPeru.API.Domain.Services
+{
+    public interface ISupervisorService
+    {
+        Task<IEnumerable<Supervisor>> ListAsync();
+        Task<SupervisorResponse> GetByIdAsync(int id);
+        Task<SupervisorResponse> SaveAsync(int personId, Supervisor supervisor);
+        Task<SupervisorResponse> UpdateAsync(int id, Supervisor supervisor);
+    }
+}
