9f51722a2a16ae5161e3331655b32304324cccd5
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-12 21:10:26 -0500

    Add Mapping

---
+++ PiensaPeru.API/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,15 @@
+﻿using PiensaPeru.API.Domain.Models;
+using PiensaPeru.API.Resources;
+
+namespace PiensaPeru.API.Mapping
+{
+    public class ModelToResourceProfile : AutoMapper.Profile
+    {
+        public ModelToResourceProfile()
+        {
+            CreateMap<Person, PersonResource>();
+
+            CreateMap<Supervisor, SupervisorResource>();
+        }
+    }
+}
---
+++ PiensaPeru.API/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using PiensaPeru.API.Domain.Models;
+using PiensaPeru.API.Resources;
+
+namespace PiensaPeru.API.Mapping
+{
+    public class ResourceToModelProfile : AutoMapper.Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<SavePersonResource, Person>();
+
+            CreateMap<SaveSupervisorResource, Supervisor>();
+        }
+    }
+}
