0a153c766e65789fac0d46f23e45b0655b2b6ecf
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-12 21:07:55 -0500

    Add Models: Person and Supervisor

---
+++ PiensaPeru.API/Domain/Models/Supervisor.cs
@@ -0,0 +1,13 @@
+﻿using PiensaPeru.API.Domain.Models;
+
+namespace PiensaPeru.API.Domain.Models
+{
+    public class Supervisor
+    {
+        public int PersonId { get; set; }
+        public Person? Person { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
