802c61ff03d964fcbabf16994cc8f9ca89686fb8
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-12 21:08:52 -0500

    Add Responses

---
+++ PiensaPeru.API/Domain/Services/Communications/PersonResponse.cs
@@ -0,0 +1,15 @@
+﻿using PiensaPeru.API.Domain.Models;
+
+namespace PiensaPeru.API.Domain.Services.Communications
+{
+    public class PersonResponse : BaseResponse<Person>
+    {
+        public PersonResponse(Person resource) : base(resource)
+        {
+        }
+
+        public PersonResponse(string message) : base(message)
+        {
+        }
+    }
+}
