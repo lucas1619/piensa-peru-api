ee23fdf4c7c9d3f245bf3a033f6ddc57c6c10a2e
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-12 21:12:15 -0500

    Add Resources

---
+++ PiensaPeru.API/Resources/SaveSupervisorResource.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PiensaPeru.API.Resources
+{
+    public class SaveSupervisorResource
+    {
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string? Password { get; set; }
+    }
+}
---
+++ PiensaPeru.API/Resources/SupervisorResource.cs
@@ -0,0 +1,9 @@
+﻿namespace PiensaPeru.API.Resources
+{
+    public class SupervisorResource
+    {
+        public int PersonId { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
