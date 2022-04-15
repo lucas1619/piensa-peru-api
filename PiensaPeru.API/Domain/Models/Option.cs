67a46a5712b33168eaba5240423b4add27ee39b0
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-15 18:06:43 -0500

    Add Option Model

---
+++ PiensaPeru.API/Domain/Models/Option.cs
@@ -0,0 +1,12 @@
+﻿namespace PiensaPeru.API.Domain.Models
+{
+    public class Option
+    {
+        public int Id { get; set; }
+        public int Description { get; set; }
+        public bool IsAnswer { get; set; }
+
+        public int QuestionId { get; set; }
+        public Question? Question { get; set; }
+    }
+}
