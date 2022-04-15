213ad0407bf15eeca88a6460e07273f97f4a6689
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-15 18:07:08 -0500

    Add Post Model

---
+++ PiensaPeru.API/Domain/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿namespace PiensaPeru.API.Domain.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? AuthorName { get; set; }
+        public string? Tag { get; set; }
+        public DateTime PostedAt { get; set; } = DateTime.Now;
+
+        public int PostTypeId { get; set; }
+        public PostType? PostType { get; set; }
+        public int ContentId { get; set; }
+        public Content? Content { get; set; }
+        public int SupervisorId { get; set; }
+        public Supervisor? Supervisor { get; set; }
+        public ICollection<Image>? Images { get; set; }
+
+    }
+}
