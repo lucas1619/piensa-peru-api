4670fbe1fa5d7a8179892241f6e282c648878b2b
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-15 18:07:02 -0500

    Add PercentageData Model

---
+++ PiensaPeru.API/Domain/Models/PercentageData.cs
@@ -0,0 +1,12 @@
+﻿namespace PiensaPeru.API.Domain.Models
+{
+    public class PercentageData
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public string? Description { get; set; }
+        public int ContentId { get; set; }
+        public Content? Content { get; set; }
+
+    }
+}
