6050b7fd16d2b49bd657be2aebf7394a5d93bfaf
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-15 18:07:22 -0500

    Add Question Model

---
+++ PiensaPeru.API/Domain/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace PiensaPeru.API.Domain.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+
+        public int QuizId { get; set; }
+        public Quiz? Quiz { get; set; }
+        public ICollection<Option>? Options { get; set; }
+    }
+}
