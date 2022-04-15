87ad7aa3dcf2e0c9b89021f2ca1f21d018fbc2a1
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-15 18:06:19 -0500

    Add Content Model

---
+++ PiensaPeru.API/Domain/Models/Content.cs
@@ -0,0 +1,14 @@
+﻿namespace PiensaPeru.API.Domain.Models
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public bool Active { get; set; } = false;
+
+        public ICollection<Post>? Posts { get; set; }
+        // public ICollection<Post>? Posts { get; set; }
+        // public ICollection<MilitantContent>? MilitantsContent { get; set; }
+        // public ICollection<Management>? Managements { get; set; }
+        // public ICollection<PercentageData>? PercentagesData { get; set; }
+    }
+}
