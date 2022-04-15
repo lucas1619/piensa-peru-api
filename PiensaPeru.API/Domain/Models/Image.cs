f7b984a8ef79e941de91d813fd6d6829e3dd53ac
gabo197 <gabriel.vasquez391@hotmail.com>
2022-04-15 18:06:36 -0500

    Add Image Model

---
+++ PiensaPeru.API/Domain/Models/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace PiensaPeru.API.Domain.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public int PostId { get; set; }
+        public Post? Post { get; set; }
+    }
+}
