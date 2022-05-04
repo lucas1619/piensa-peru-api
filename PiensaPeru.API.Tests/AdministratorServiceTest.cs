e8870a0f64e957ec2f45a7ab758bd892949f2c64
Emanuel Josias Ticona Calsín <josias876@hotmail.com>
2022-05-04 13:10:52 -0500

    fixing Administrator Test

---
+++ PiensaPeru.API.Tests/AdministratorServiceTest.cs
@@ -0,0 +1,201 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using PiensaPeru.API.Controllers;
+using PiensaPeru.API.Domain.Models.AdministratorBoundedContextModels;
+using PiensaPeru.API.Domain.Persistence.Repositories;
+using PiensaPeru.API.Domain.Persistence.Repositories.AdministratorBoundedContextRespositories;
+using PiensaPeru.API.Domain.Services.AdministratorBoundedContextServices;
+using PiensaPeru.API.Domain.Services.Communications.AdministratorBoundedContextCommunications;
+using PiensaPeru.API.Resources.AdministratorBoundedContextResources;
+using PiensaPeru.API.Services.AdministratorBoundedContextServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiensaPeru.API.Tests
+{
+    public class AdministratorServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task GetByIdAsyncWhenNoAdministratorFoundReturnsAdministratorNotFoundResponse()
+        {
+            // Arrange
+            var mockAdministratorRepository = GetDefaultIAdministratorRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var administratorId = 1;
+            mockAdministratorRepository.Setup(r => r.FindById(administratorId))
+                .Returns(Task.FromResult<Administrator>(null));
+
+            var service = new AdministratorService(mockAdministratorRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            AdministratorResponse result = await service.GetByIdAsync(administratorId);
+            var message = result.Message;
+
+            // Assert
+            message.Should().Be("Administrator not found");
+
+        }
+
+        [Test]
+        public async Task GetByIdAsyncWhenAdministratorFoundReturnsSuccess()
+        {
+            // Arrange
+            var mockAdministratorRepository = GetDefaultIAdministratorRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var administratorId = 1;
+            Administrator t = new()
+            {
+                Id = 1,
+                FirstName = "Miguel",
+                LastName = "Pep",
+                Email = "mikypep69@hotmail.com",
+                Password = "furrykawai"
+            };
+            mockAdministratorRepository.Setup(r => r.FindById(administratorId))
+                .Returns(Task.FromResult<Administrator>(t));
+
+            var service = new AdministratorService(mockAdministratorRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            AdministratorResponse result = await service.GetByIdAsync(administratorId);
+            var success = result.Success;
+
+            // Assert
+            success.Should().Be(true);
+
+        }
+
+        [Test]
+        public async Task SaveAsyncWhenAdministratorIsSentSuccessfully()
+        {
+            // Arrange
+            var mockAdministratorRepository = GetDefaultIAdministratorRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            Administrator t = new()
+            {
+                Id = 1,
+                FirstName = "Miguel",
+                LastName = "Pep",
+                Email = "mikypep69@hotmail.com",
+                Password = "furrykawai"
+            };
+            mockAdministratorRepository.Setup(r => r.AddAsync(t))
+                .Returns(Task.FromResult<Administrator>(t));
+
+            var service = new AdministratorService(mockAdministratorRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            AdministratorResponse result = await service.SaveAsync(t);
+            var success = result.Success;
+
+            // Assert
+            success.Should().Be(true);
+
+        }
+
+        [Test]
+        public async Task UpdateAsyncWhenAdministratorIsSentSuccessfully()
+        {
+            // Arrange
+            Mock<IAdministratorRepository> administratorRepository = new Mock<IAdministratorRepository>();
+            var mockAdministratorRepository = GetDefaultIAdministratorRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            var service = new AdministratorService(administratorRepository.Object, mockUnitOfWork.Object);
+            Administrator t = new()
+            {
+                Id = 1,
+                FirstName = "Miguel",
+                LastName = "Pep",
+                Email = "mikypep69@hotmail.com",
+                Password = "furrykawai"
+            };
+            //var mockAdministratorService = GetDefaultIAdministratorServiceInstance(); ;
+            //var mockMapper = GetDefaultIMapperInstance();
+
+            //mockAdministratorRepository.Setup(r => r.FindById(t.Id)).ReturnsAsync(t);
+            administratorRepository.Setup(r => r.AddAsync(t));
+
+            //administratorRepository.Setup(r => r.FindById(t.Id)).ReturnsAsync(t);
+
+            var itemId = t.Id;
+            Administrator itemToUpdate = new()
+            {
+                Id = 1,
+                FirstName = "Miguel",
+                LastName = "Pep",
+                Email = "mikypep69@gmail.com",
+                Password = "elpepito"
+            };
+
+            //var controller = new AdministratorsController(mockAdministratorService.Object, mockMapper.Object);
+            
+
+            // Act
+            AdministratorResponse result = await service.UpdateAsync(itemId, itemToUpdate);
+
+            // Assert
+            //Assert.AreEqual()
+            //result.Should().BeOfType<NoContentResult>();
+            result.Success.Should().Be(true);
+            
+
+        }
+
+        [Test]
+        public async Task DeleteAsyncWhenAdministratorIsSentSuccessfully()
+        {
+            // Arrange
+            var mockAdministratorRepository = GetDefaultIAdministratorRepositoryInstance();
+            var mockUnitOfWork = GetDefaultIUnitOfWorkInstance();
+            Administrator t = new()
+            {
+                Id = 1,
+                FirstName = "Miguel",
+                LastName = "Pep",
+                Email = "mikypep69@hotmail.com",
+                Password = "furrykawai"
+            };
+
+            mockAdministratorRepository.Setup(r => r.FindById(t.Id)).ReturnsAsync(t);
+
+            var service = new AdministratorService(mockAdministratorRepository.Object, mockUnitOfWork.Object);
+
+            // Act
+            AdministratorResponse result = await service.DeleteAsync(t.Id);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
+        }
+
+        private Mock<IAdministratorRepository> GetDefaultIAdministratorRepositoryInstance()
+        {
+            return new Mock<IAdministratorRepository>();
+        }
+
+        private Mock<IUnitOfWork> GetDefaultIUnitOfWorkInstance()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+
+        private Mock<IAdministratorService> GetDefaultIAdministratorServiceInstance()
+        {
+            return new Mock<IAdministratorService>();
+        }
+
+        private Mock<IMapper> GetDefaultIMapperInstance()
+        {
+            return new Mock<IMapper>();
+        }
+    }
+}
--- PiensaPeru.API.Tests/SupervisorServiceTest.cs
+++ PiensaPeru.API.Tests/SupervisorServiceTest.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+﻿    using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using PiensaPeru.API.Domain.Models;
